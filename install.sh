782b1de432b5906cccf7a130212243fe3a180c54
Zoltán Reegn <zoltan.reegn@gmail.com>
2021-08-16 23:24:16 +0200

    Add installer script for convenience

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# pre-create some directories, so stow doesn't softlink
+# the entire directory, just the contents
+mkdir -p "$HOME/bin" "$HOME/.config/direnv/lib" "$HOME/.vim" "$HOME/.config"
+
+for dir in */; do
+	stow -R -v --target "$HOME" "${dir%*/}"
+done
