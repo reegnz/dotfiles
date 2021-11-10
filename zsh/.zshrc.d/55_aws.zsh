eff57c4176da52da008ffe07770aa33eb4c57dc3
Zoltán Reegn <zoltan.reegn@gmail.com>
2021-11-10 18:56:12 +0100

    zsh: improve aws script
    * add -u --unset flag
    * add -c --current flag
    * add bash completion for flags and profiles

--- zsh/.zshrc.d/55_aws.zsh
+++ zsh/.zshrc.d/55_aws.zsh
@@ -1,26 +1,94 @@
-function asp() {
-  if [[ -z "$1" ]]; then
-    AWS_PROFILE="$(aws_select_profile)"
-    export AWS_PROFILE
-  fi
+#shellcheck disable=SC2148
 
-  if [[ "$1" = "-u" ]]; then
-    unset AWS_PROFILE
-    echo "Unset AWS profile."
+alias aws-edit-config='vim -c "cd ~/.aws" -c "Rg"'
+
+aws-switch-profile() {
+  if [[ $# -eq 0 ]]; then
+    AWS_PROFILE="$(__aws_select_profile)"
+    export AWS_PROFILE
     return
   fi
+
+  while [[ $# -gt 0 ]]; do
+    key="$1"
+    case ${key} in
+      -u|--unset)
+        unset AWS_PROFILE
+        echo "Unset AWS profile." >&2
+        return
+        ;;
+      -c|--current)
+        echo "${AWS_PROFILE}"
+        return
+        ;;
+      -*)
+        echo "Unknown flag: ${key}" >&2
+        return 1
+        ;;
+      *)
+        export AWS_PROFILE="${key}"
+        return
+        ;;
+    esac
+  done
 }
 
-alias aws_edit_config='vim -c "cd ~/.aws" -c "Rg"'
-
-function aws_select_profile() {
-    aws_profiles |
-      fzf --reverse \
-        --preview 'awk '"'"'/\[(profile )?{}]/{keep=1;print;next};/\[.*]/{keep=0};keep{print}'"'"' "${AWS_CONFIG_FILE:-$HOME/.aws/config}"|bat -l ini --color=always -p'
+_aws-switch-profile() {
+    COMPREPLY=()
+    local current=${COMP_WORDS[COMP_CWORD]}
+    if [ "${COMP_CWORD}" -eq 2 ]; then
+      return
+    fi
+    case ${current} in
+      -*)
+        flags="-u --unset -c --current"
+        #shellcheck disable=SC2207
+        COMPREPLY=( $(compgen -W "$flags" -- "${current}") )
+        return
+        ;;
+      *)
+        profiles=( $(aws-profiles) )
+        COMPREPLY=( $( compgen -W "${profiles[*]}" -- "${current}" ) )
+        return
+        ;;
+    esac
 }
+complete -F _aws-switch-profile aws-switch-profile
 
-function aws_profiles() {
+alias asp=aws-switch-profile
+complete -F _aws-switch-profile asp
+
+__aws_select_profile() (
+    aws-profiles |
+      fzf --reverse --preview "$(declare -f __aws_print_profile); __aws_print_profile {}"
+)
+
+__aws_print_profile() (
+  local profile=$1
+  local pattern="\\\[(profile )?${profile}]"
+  awk -v pattern="${pattern}" '
+    $0 ~ pattern {
+      keep = 1
+      print
+      next
+    }
+    /\[.*]/ {
+      keep = 0;
+    }
+    keep' "${AWS_CONFIG_FILE:-$HOME/.aws/config}" |
+    if type bat &>/dev/null; then
+      bat -l ini --color=always --decorations=never
+    else
+      cat
+    fi
+)
+
+aws-profiles() (
+  local file="${AWS_CONFIG_FILE:-$HOME/.aws/config}"
+  if [ ! -f "${file}" ]; then
+    return
+  fi
   awk -F'[][ ]' \
     '/\[profile .*]/{print $3;next}; /\[.*]/{print $2;next}' \
-    "${AWS_CONFIG_FILE:-$HOME/.aws/config}"
-}
+    "${file}"
+)
