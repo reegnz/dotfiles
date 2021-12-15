b9561a1fe7a5ad26dfcbbd137d0cf35416ae3858
Zoltán Reegn <zoltan.reegn@gmail.com>
2021-12-15 11:42:55 +0100

    [hammerspoon] minor improvements

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -6,20 +6,37 @@ hs.loadSpoon("SpoonInstall")
 Install = spoon.SpoonInstall
 
 Install:andUse("ReloadConfiguration", {
+  hotkeys = {
+    reloadConfiguration = { {"Ctrl"}, ";"}
+  },
   start = true,
+})
+
+Install:andUse("FadeLogo", { start = true })
+
+Install:andUse("RoundedCorners", {
+  config = {
+    radius = 10
+  },
+  start = true
 })
 
 Safari = "com.apple.Safari"
 Chrome = 'com.google.Chrome'
 Firefox = 'org.mozilla.firefox'
 Zoom = 'us.zoom.xos'
-DefaultBrowser = Chrome
+Slack = 'com.tinyspeck.slackmacgap'
+
+DefaultBrowser = Firefox
+WorkBrowser = Chrome
+
+require("private")
 
 Install:andUse("URLDispatcher", {
   config = {
     url_patterns = {
 	    { "https?://.*%.zoom%.us", Zoom },
-	    { "https?://.*github.com", Firefox },
+	    { "https?://.*slack.com", Slack },
     },
     default_handler = DefaultBrowser,
   },
