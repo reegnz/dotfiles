575a239a8056a4c6e00ad4905d4b2aacf9f5dfa4
Zoltán Reegn <zoltan.reegn@gmail.com>
2022-06-29 11:43:08 +0200

    hammerspoon: bootstrap SpoonInstall

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,6 +1,28 @@
-# allow `hs` cli
+logger = hs.logger.new('init', 'debug')
+-- allow `hs` cli
 require("hs.ipc")
 hs.ipc.cliInstall()
+
+-- Install SpoonInstall if needed
+local spoon_install_path = hs.spoons.resourcePath("Spoons")
+if
+  not pcall(function()
+    logger.i("Checking if SpoonInstall is installed...")
+    hs.fs.dir(spoon_install_path .. "/SpoonInstall.spoon")
+  end)
+then
+  logger.i("Installing SpoonInstall...")
+  status, body, headers = hs.http.get("https://github.com/Hammerspoon/Spoons/raw/master/Spoons/SpoonInstall.spoon.zip", nil)
+  local zipfile = spoon_install_path .. "/SpoonInstall.spoon.zip"
+  hs.fs.mkdir(spoon_install_path)
+  io.open(zipfile, "w"):write(body):close()
+  hs.execute(string.format("/usr/bin/unzip -d %s %s", spoon_install_path, zipfile))
+
+  hs.execute(string.format("/bin/rm '%s'", zipfile))
+  logger.i("Installed SpoonInstall!")
+else
+  logger.i("SpoonInstall already installed!")
+end
 
 hs.loadSpoon("SpoonInstall")
 Install = spoon.SpoonInstall
