947d2c25d843072e11b550e32dc21dab974e7cf2
Zoltán Reegn <zoltan.reegn@gmail.com>
2022-08-16 18:49:43 +0200

    hammerspoon

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,46 +1,40 @@
 logger = hs.logger.new('init', 'debug')
--- allow `hs` cli
-require("hs.ipc")
 hs.ipc.cliInstall()
 
 -- Install SpoonInstall if needed
-local spoon_install_path = hs.spoons.resourcePath("Spoons")
-if
-  not pcall(function()
-    logger.i("Checking if SpoonInstall is installed...")
-    hs.fs.dir(spoon_install_path .. "/SpoonInstall.spoon")
-  end)
-then
-  logger.i("Installing SpoonInstall...")
-  status, body, headers = hs.http.get("https://github.com/Hammerspoon/Spoons/raw/master/Spoons/SpoonInstall.spoon.zip", nil)
-  local zipfile = spoon_install_path .. "/SpoonInstall.spoon.zip"
-  hs.fs.mkdir(spoon_install_path)
-  io.open(zipfile, "w"):write(body):close()
-  hs.execute(string.format("/usr/bin/unzip -d %s %s", spoon_install_path, zipfile))
-
-  hs.execute(string.format("/bin/rm '%s'", zipfile))
-  logger.i("Installed SpoonInstall!")
+local spoon_install_path = hs.configdir .. "/Spoons"
+if pcall(function()
+	logger.i("Checking if SpoonInstall is installed...")
+	hs.fs.dir(spoon_install_path .. "/SpoonInstall.spoon")
+end) then
+	logger.i("SpoonInstall already installed!")
 else
-  logger.i("SpoonInstall already installed!")
+	logger.i("Installing SpoonInstall...")
+	status, body, headers = hs.http.get("https://github.com/Hammerspoon/Spoons/raw/master/Spoons/SpoonInstall.spoon.zip",
+		nil)
+	local zipfile = spoon_install_path .. "/SpoonInstall.spoon.zip"
+	hs.fs.mkdir(spoon_install_path)
+	io.open(zipfile, "w"):write(body):close()
+	hs.execute(string.format("/usr/bin/unzip -d %s %s", spoon_install_path,
+		zipfile))
+
+	hs.execute(string.format("/bin/rm '%s'", zipfile))
+	logger.i("Installed SpoonInstall!")
 end
 
 hs.loadSpoon("SpoonInstall")
 Install = spoon.SpoonInstall
 
 Install:andUse("ReloadConfiguration", {
-  hotkeys = {
-    reloadConfiguration = { {"Ctrl"}, ";"}
-  },
-  start = true,
+	hotkeys = { reloadConfiguration = { { "Ctrl" }, ";" } },
+	start = true
 })
 
-Install:andUse("FadeLogo", { start = true })
-
 Install:andUse("RoundedCorners", {
-  config = {
-    radius = 10
-  },
-  start = true
+	config = {
+		radius = 10
+	},
+	start = true
 })
 
 Safari = "com.apple.Safari"
@@ -52,15 +46,23 @@ Slack = 'com.tinyspeck.slackmacgap'
 DefaultBrowser = Firefox
 WorkBrowser = Chrome
 
-require("private")
-
 Install:andUse("URLDispatcher", {
-  config = {
-    url_patterns = {
-	    { "https?://.*%.zoom%.us", Zoom },
-	    { "https?://.*%.slack%.com", Slack },
-    },
-    default_handler = DefaultBrowser,
-  },
-  start = true,
+	config = {
+		url_patterns = {
+			{ "https?://.*%.zoom%.us", Zoom },
+			{ "https?://.*%.slack%.com", Slack },
+
+			-- example redirect work stuff to work browser
+			{ "https?://.*%.aws%.amazon.com", WorkBrowser },
+			{ "https?://login%.microsoftonline%.com", WorkBrowser },
+			{ "https?://.*%.atlassian.net", WorkBrowser },
+			{ "https?://.*%.datadoghq.com", WorkBrowser },
+			{ "https?://.*%.pagerduty.com", WorkBrowser },
+
+		},
+		default_handler = DefaultBrowser
+	},
+	start = true
 })
+
+Install:andUse("FadeLogo", { start = true })
