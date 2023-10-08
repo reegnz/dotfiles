2c6f6735ba23bb8825aa562ca730414838412e68
Zoltán Reegn <zoltan.reegn@gmail.com>
2023-10-08 13:54:52 +0200

    [hammerspoon] snap

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,47 +1,48 @@
-logger = hs.logger.new('init', 'debug')
+local logger = hs.logger.new("init", "debug")
 hs.ipc.cliInstall()
 
 -- Install SpoonInstall if needed
 local spoon_install_path = hs.configdir .. "/Spoons"
-if pcall(function()
-	logger.i("Checking if SpoonInstall is installed...")
-	hs.fs.dir(spoon_install_path .. "/SpoonInstall.spoon")
-end) then
+if
+	pcall(function()
+		logger.i("Checking if SpoonInstall is installed...")
+		hs.fs.dir(spoon_install_path .. "/SpoonInstall.spoon")
+	end)
+then
 	logger.i("SpoonInstall already installed!")
 else
 	logger.i("Installing SpoonInstall...")
-	status, body, headers = hs.http.get("https://github.com/Hammerspoon/Spoons/raw/master/Spoons/SpoonInstall.spoon.zip",
-		nil)
+	local status, body, headers =
+		hs.http.get("https://github.com/Hammerspoon/Spoons/raw/master/Spoons/SpoonInstall.spoon.zip", nil)
 	local zipfile = spoon_install_path .. "/SpoonInstall.spoon.zip"
 	hs.fs.mkdir(spoon_install_path)
 	io.open(zipfile, "w"):write(body):close()
-	hs.execute(string.format("/usr/bin/unzip -d %s %s", spoon_install_path,
-		zipfile))
+	hs.execute(string.format("/usr/bin/unzip -d %s %s", spoon_install_path, zipfile))
 
 	hs.execute(string.format("/bin/rm '%s'", zipfile))
 	logger.i("Installed SpoonInstall!")
 end
 
 hs.loadSpoon("SpoonInstall")
-Install = spoon.SpoonInstall
+local Install = spoon.SpoonInstall
 
 Install:andUse("ReloadConfiguration", {
 	hotkeys = { reloadConfiguration = { { "Ctrl" }, ";" } },
-	start = true
+	start = true,
 })
 
 Install:andUse("RoundedCorners", {
 	config = {
-		radius = 10
+		radius = 10,
 	},
-	start = true
+	start = true,
 })
 
 Safari = "com.apple.Safari"
-Chrome = 'com.google.Chrome'
-Firefox = 'org.mozilla.firefox'
-Zoom = 'us.zoom.xos'
-Slack = 'com.tinyspeck.slackmacgap'
+Chrome = "com.google.Chrome"
+Firefox = "org.mozilla.firefox"
+Zoom = "us.zoom.xos"
+Slack = "com.tinyspeck.slackmacgap"
 
 DefaultBrowser = Firefox
 WorkBrowser = Chrome
@@ -65,9 +66,9 @@ Install:andUse("URLDispatcher", {
 
 
 		},
-		default_handler = DefaultBrowser
+		default_handler = DefaultBrowser,
 	},
-	start = true
+	start = true,
 })
 
 Install:andUse("FadeLogo", { start = true })
