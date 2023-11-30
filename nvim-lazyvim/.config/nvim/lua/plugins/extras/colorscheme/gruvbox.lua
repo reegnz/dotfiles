bf7cd7a92eac97fb7fd6ce2d63268d1523f33eb9
Zoltán Reegn <zoltan.reegn@gmail.com>
2023-11-30 09:11:18 +0100

    [nvim] update lazyvim dependencies

--- nvim-lazyvim/.config/nvim/lua/plugins/extras/colorscheme/gruvbox.lua
+++ nvim-lazyvim/.config/nvim/lua/plugins/extras/colorscheme/gruvbox.lua
@@ -1,9 +1,15 @@
 return {
-  "ellisonleao/gruvbox.nvim",
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      contrast = "hard",
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "gruvbox",
     },
+    dependencies = "gruvbox.nvim",
   },
 }
