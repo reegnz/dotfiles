6eb8e33caf55210245230e58a0108c1dbde6609f
Zoltán Reegn <zoltan.reegn@gmail.com>
2025-02-14 16:16:28 +0100

    wezterm: disable tabs

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -8,6 +8,7 @@ local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
 config.color_scheme = "Gruvbox dark, hard (base16)"
+config.enable_tab_bar = false
 
 -- and finally, return the configuration to wezterm
 return config
