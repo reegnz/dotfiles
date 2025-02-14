a93205d72f3ce4ba91cb44439e8f6ff296a7f8e2
Zoltán Reegn <zoltan.reegn@gmail.com>
2025-02-14 16:12:48 +0100

    wezterm: initial config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "Gruvbox dark, hard (base16)"
+
+-- and finally, return the configuration to wezterm
+return config
