9ba8dd6be6603aaf3c98d0765e421bc6229b94c7
Zoltán Reegn <zoltan.reegn@gmail.com>
2025-03-18 09:54:57 +0100

    feat(direnv): direnv toggle

--- direnv/.zshrc.d/55_direnv.zsh
+++ direnv/.zshrc.d/55_direnv.zsh
@@ -2,13 +2,42 @@
 if (( ! ${+commands[direnv]} )); then
   return
 fi
-direnv_hook_cache="${ZSH_CACHE_DIR}/direnv.zsh"
-if [[ "$commands[direnv]" -nt "$direnv_hook_cache" || ! -s "$direnv_hook_cache" ]]; then
-  direnv hook zsh >| "$direnv_hook_cache"
-  zcompile "$direnv_hook_cache"
-fi
-if [ -z "${DIRENV_DISABLE:-}" ]; then
-  source "$direnv_hook_cache"
-fi
-unset direnv_hook_cache
 
+_direnv_hook() {
+  trap -- '' SIGINT
+  eval "$("/opt/homebrew/bin/direnv" export zsh)"
+  trap - SIGINT
+}
+
+direnv_hook_disable() {
+  typeset -ag precmd_functions
+  if (( ${precmd_functions[(I)_direnv_hook]} )); then
+    precmd_functions=(${precmd_functions:#_direnv_hook})
+  fi
+  typeset -ag chpwd_functions
+  if (( ${chpwd_functions[(I)_direnv_hook]} )); then
+    chpwd_functions=(${chpwd_functions#_direnv_hook})
+  fi
+}
+
+direnv_hook_enable() {
+  typeset -ag precmd_functions
+  if (( ! ${precmd_functions[(I)_direnv_hook]} )); then
+    precmd_functions=(_direnv_hook $precmd_functions)
+  fi
+  typeset -ag chpwd_functions
+  if (( ! ${chpwd_functions[(I)_direnv_hook]} )); then
+    chpwd_functions=(_direnv_hook $chpwd_functions)
+  fi
+}
+
+direnv_hook_toggle() {
+  typeset -ag precmd_functions
+  if (( ${precmd_functions[(I)_direnv_hook]} )); then
+    direnv_hook_disable
+  else
+    direnv_hook_enable
+  fi
+}
+
+direnv_hook_enable
