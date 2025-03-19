a33d676e103a94cf195b4cd16701818645b0523c
Zoltán Reegn <zoltan.reegn@gmail.com>
2025-03-19 20:12:22 +0100

    feat(asdf): move to new version

--- asdf/.zshrc.d/55_asdf.zsh
+++ asdf/.zshrc.d/55_asdf.zsh
@@ -2,11 +2,4 @@
 if (( ! ${+commands[asdf]} )); then
   return
 fi
-if [ -n "${ASDF_DIR:-}" ]; then
-  return
-fi
-if [ ! -f "$HOMEBREW_PREFIX/opt/asdf/libexec/asdf.sh" ]; then
-  return
-fi
-. ${HOMEBREW_PREFIX}/opt/asdf/libexec/asdf.sh
-export ASDFZF_VERSION_KEEP_kubectl="semver-no-prerelease"
+path=("$XDG_HOME/.asdf/shims" $path)
