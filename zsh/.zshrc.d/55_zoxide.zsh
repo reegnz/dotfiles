88840312e3e2709886e0ebcf330238887eb24e9e
Zoltán Reegn <zoltan.reegn@gmail.com>
2025-03-19 20:13:32 +0100

    fix(zoxide): add zcompile for hook

--- zsh/.zshrc.d/55_zoxide.zsh
+++ zsh/.zshrc.d/55_zoxide.zsh
@@ -5,6 +5,7 @@ fi
 zoxide_hook_cache="${ZSH_CACHE_DIR}/zoxide.zsh"
 if [[ "$commands[zoxide]" -nt "$zoxide_hook_cache" || ! -s "$zoxide_hook_cache" ]]; then
   zoxide init zsh >| "$zoxide_hook_cache"
+  zcompile "$zoxide_hook_cache"
 fi
 source "$zoxide_hook_cache"
 unset zoxide_hook_cache
