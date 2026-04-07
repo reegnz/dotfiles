396cc052f97cff5a354ae14f24497db5894b95ff
Zoltán Reegn <zoltan.reegn@gmail.com>
2026-04-07 10:16:54 +0200

    chore(nvim): upgrade, refactor avante config

--- nvim-lazyvim/.config/nvim/lua/plugins/avante.lua
+++ nvim-lazyvim/.config/nvim/lua/plugins/avante.lua
@@ -1,81 +1,27 @@
 return {
   {
     "yetone/avante.nvim",
-    event = "VeryLazy",
     opts = {
-      hints = {
-        enabled = false,
-      },
-      provider = "ollama",
-      ollama = {
-        model = "codellama:7b",
-      },
-      file_selector = {
-        provider = "fzf",
-        provider_opts = {},
+      provider = "cursor",
+      mode = "agentic",
+      acp_providers = {
+        ["cursor"] = {
+          command = "cursor-agent",
+          args = { "acp" },
+          auth_method = "cursor_login",
+          env = {
+            HOME = os.getenv("HOME"),
+            PATH = os.getenv("PATH"),
+          },
+        },
       },
     },
-    build = "make",
     dependencies = {
-      "nvim-treesitter/nvim-treesitter",
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
-      "stevearc/dressing.nvim",
-      "ibhagwan/fzf-lua",
-    },
-  },
-  {
-    "folke/which-key.nvim",
-    optional = true,
-    opts = {
-      spec = {
-        { "<leader>a", group = "ai" },
-      },
     },
   },
   {
     "MeanderingProgrammer/render-markdown.nvim",
-    ft = { "Avante" },
-  },
-  {
-    "saghen/blink.cmp",
-    lazy = true,
-    dependencies = {
-      "saghen/blink.compat",
-    },
-    opts = {
-      sources = {
-        default = {
-          "avante_commands",
-          "avante_mentions",
-          "avante_files",
-        },
-        compat = {
-          "avante_commands",
-          "avante_mentions",
-          "avante_files",
-        },
-        providers = {
-          avante_commands = {
-            name = "avante_commands",
-            module = "blink.compat.source",
-            score_offset = 90,
-            opts = {},
-          },
-          avante_files = {
-            name = "avante_files",
-            module = "blink.compat.source",
-            score_offset = 100,
-            opts = {},
-          },
-          avante_mentions = {
-            name = "avante_mentions",
-            module = "blink.compat.source",
-            score_offset = 1000,
-            opts = {},
-          },
-        },
-      },
-    },
   },
 }
