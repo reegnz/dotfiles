3c5329b6862d7a731b147dfdba87378a0a6e9797
Zoltán Reegn <zoltan.reegn@gmail.com>
2026-05-19 15:08:27 +0200

    feat(nvim): add which-key group for vim-slime

--- nvim-lazyvim/.config/nvim/lua/plugins/slime.lua
+++ nvim-lazyvim/.config/nvim/lua/plugins/slime.lua
@@ -1,43 +1,62 @@
 return {
-  "jpalardy/vim-slime",
-  init = function()
-    vim.g.slime_target = "neovim"
-    vim.g.slime_no_mappings = 1
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "markdown",
-      callback = function()
-        vim.b.slime_cell_delimiter = "```"
-      end,
-    })
-  end,
-  keys = {
-    {
-      "<leader>rr",
-      "<Plug>SlimeRegionSend",
-      mode = "x",
-      desc = "REPL: slime send selection",
-      remap = true,
+  {
+    "jpalardy/vim-slime",
+    init = function()
+      vim.g.slime_target = "neovim"
+      vim.g.slime_no_mappings = 1
+      vim.g.slime_bracketed_paste = 0
+      vim.g.slime_preserve_curline = 0
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function()
+          vim.b.slime_cell_delimiter = "```"
+        end,
+      })
+    end,
+    keys = {
+      {
+        "<leader>rr",
+        "<Plug>SlimeRegionSend",
+        mode = "x",
+        desc = "slime: send selection",
+      },
+      {
+        "<leader>rr",
+        "<Plug>SlimeLineSend",
+        mode = "n",
+        desc = "slime: send line",
+      },
+      {
+        "<leader>rp",
+        "<Plug>SlimeParagraphSend",
+        mode = "n",
+        desc = "slime: send paragraph",
+      },
+      {
+        "<leader>rc",
+        "<Plug>SlimeSendCell",
+        mode = "n",
+        desc = "slime: send fenced cell",
+      },
+      {
+        -- Optional: A manual submit key just in case you ever want to
+        -- review the pasted text in the terminal before sending it.
+        "<leader>rs",
+        function()
+          vim.fn["slime#send"]("\r")
+        end,
+        mode = "n",
+        desc = "slime: Submit (Send Enter)",
+      },
     },
-    {
-      "<leader>rl",
-      "<Plug>SlimeLineSend",
-      mode = "n",
-      desc = "REPL: slime send line",
-      remap = true,
-    },
-    {
-      "<leader>rp",
-      "<Plug>SlimeParagraphSend",
-      mode = "n",
-      desc = "REPL: Slime send paragraph",
-      remap = true,
-    },
-    {
-      "<leader>rc",
-      "<Plug>SlimeSendCell",
-      mode = "n",
-      desc = "REPL: slime send fenced cell",
-      remap = true,
+  },
+  {
+    "folke/which-key.nvim",
+    optional = true,
+    opts = {
+      spec = {
+        { "<leader>r", group = "vim-slime", mode = { "n", "x" } },
+      },
     },
   },
 }
